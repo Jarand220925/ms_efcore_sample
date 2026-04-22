b4afaa99e21368b0cdc1ff13ca236f404f7caa44
Jarand220925 <jarandkurs@gmail.com>
2026-04-22 13:24:31 +0200

    La til en bedre implementasjon av serialisering til GeoJson, og en eiendoms-modell.

---
+++ ms_efcore_sample/classes/Dtos/CoordinateGeojsonDto.cs
@@ -0,0 +1,28 @@
+﻿using ms_efcore_sample.interfaces;
+using ms_efcore_sample.models;
+using NetTopologySuite.Features;
+
+namespace ms_efcore_sample.classes.Dtos;
+
+public class CoordinateGeojsonDto : IGeojsonDto
+{
+    public Feature Feature { get; set; }
+    public FeatureCollection FeatureCollection { get; set; }
+    public CoordinateGeojsonDto(Coordinate coordinate)
+    {
+        var id = coordinate.CoordinateId;
+        var longitude = coordinate.Longitude;
+        var latitude = coordinate.Latitude;
+        var Epsg = coordinate.EPSG;
+        
+        var attributes = new AttributesTable();
+        attributes.Add("Id", id);
+        attributes.Add("EPSG", Epsg);
+        
+        Feature = new Feature(coordinate.GeographyPoint, attributes);
+        //FeatureCollection = new FeatureCollection { feature };
+        
+    }
+
+    
+}
---
+++ ms_efcore_sample/models/Eiendom.cs
@@ -0,0 +1,12 @@
+﻿namespace ms_efcore_sample.models;
+
+public class Eiendom
+{
+    public int EiendomId { get; set; }
+    
+    public int KommuneId { get; set; }
+    
+    public int CoordinateId { get; set; }
+    
+    public required string Type { get; set; }
+}
